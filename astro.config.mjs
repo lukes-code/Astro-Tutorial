f4fd5342913ba0fc919c520c0c33c8588209b3f3
Luke Sterry <lukesterry@gmail.com>
2024-01-17 21:51:25 +0000

    Finish tutorial

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,9 @@
 import { defineConfig } from 'astro/config';
 
+import preact from "@astrojs/preact";
+
 // https://astro.build/config
 export default defineConfig({
-    site: "https://sunny-pasca-13b4ff.netlify.app/"
+  site: "https://sunny-pasca-13b4ff.netlify.app/",
+  integrations: [preact()]
 });
